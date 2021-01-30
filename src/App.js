93c033752a0ca01ce740dfa546f2ec83a9f3d82e
RayRCaringal <rayrcaringal@gmail.com>
2021-01-30 17:55:56 -0500

    Fixed Node Grid

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { Col, Container, Form, Navbar, Row, Button, Image } from 'react-bootstrap'
+import { Col, Container, Form, Navbar, Row, Button, Image, Jumbotron } from 'react-bootstrap'
 import Grid from './components/Grid'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/App.scss'
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className = "App">
-      
-          <Navbar bg = "dark" variant = "dark">
-            <Container>
-              <Navbar.Brand> Shortest Path Simulator</Navbar.Brand>
-            </Container>
-            <Button className = "mx-auto">Start</Button>
-
-          </Navbar>
-    
-
-               <Container fluid = "sm" className = "p-1">
-                 <Grid></Grid>
-               </Container>
-
-
- 
-
+      <Navbar bg = "dark" variant = "dark">
+        <Container>
+          <Navbar.Brand> Shortest Path Simulator</Navbar.Brand>
+        </Container>
+        <Button className = "mx-auto">Start</Button>
+      </Navbar>
+      <Jumbotron className = "p-1">
+        <Grid></Grid>
+      </Jumbotron>
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,9 +4,16 @@ import React from "react"
 import {useEffect, useRef, useState } from "react"
 import "../style/grid.css"    
 let arr;
-
+let ctx
 const Grid = () => {
 
+
+    const updateArr = (x,y) =>{
+        ctx.fillRect(x*(Math.floor((window.innerWidth*2)/w))+3,
+            y*(Math.floor((window.innerHeight*2)/h))+3,
+            Math.floor((window.innerWidth*2)/w)-6,
+            Math.floor((window.innerHeight*2)/h)-6)
+    }
 
     const canvasRef = useRef(null)
     const contextRef = useRef(null)
@@ -19,7 +26,9 @@ const Grid = () => {
     const h = Math.floor((window.innerHeight -6 * size)/size);
 
     const n = Math.floor(window.innerWidth/size)
-    const scaling = (n-6)/2
+    const scalingX = Math.floor((window.innerWidth*2)/w)-1.5
+    const scalingY = Math.floor((window.innerHeight*2)/h)-1
+
 
     //Run only once 
     useEffect(() => {
@@ -31,55 +40,52 @@ const Grid = () => {
 
     
     useEffect(() => {
-        console.log("In Use Effect")
+        //console.log("In Use Effect")
         //Set Canvas Dimensions 
         const canvas = canvasRef.current
         canvas.width = window.innerWidth*2
         canvas.height = window.innerHeight*2
         canvas.style.width = window.innerWidth
         canvas.style.height = window.innerHeight
-        const ctx = canvas.getContext("2d")
+        ctx = canvas.getContext("2d")
         ctx.scale(2,2)
         contextRef.current = ctx
         ctx.lineWidth = 2;
         ctx.fillStyle = "grey";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
         for(let i = 0; i < arr.length; i++){
             for(let j = 0; j < arr[0].length; j++){
-                if( arr[i][j] == 'a'){
-                    ctx.fillStyle = "#F3F3F3" 
-                }else{
-                    console.log("Here")
-                    ctx.fillStyle = "#1B2631"
-                }
-                /*
-                if(i < 2 && j < 2){
-                    console.log(((i*(n/size))+" , "+(j * (n/size))+" , "+((n/size)-6)))
-
-                }
-                */
-                ctx.fillRect(i*n,j * n,n-6, n-6)
+                ctx.fillStyle = "#F3F3F3" 
+                ctx.fillRect(
+                i*(Math.floor(canvas.width/w))+3,
+                j*(Math.floor(canvas.height/h))+3
+                ,Math.floor(canvas.width/w)-6,
+                 Math.floor(canvas.height/h)-6
+                 )
             }
         }
+        ctx.fillStyle = "#1B2631"
         console.log("Create Rect")
         
-    },)
+    },[w,h])
 
     //When Mouse is clicked take the current positions to calculate which part of the Grid to convert to an obstacle
     const startDraw = ({nativeEvent}) =>{
         const {offsetX, offsetY} = nativeEvent
         console.log(offsetX + " , " + offsetY )
-        //console.log(Math.floor(offsetX/((n-6)/2))+ " , " + Math.floor(offsetY/((n-6)/2)))
-        arr[Math.floor(offsetX/scaling)][Math.floor(offsetY/scaling)] = 'b';
-        console.log(arr)
+        const x = Math.floor(offsetX/scalingX)
+        const y = Math.floor(offsetY/scalingY)
+        //console.log(x+ " , " + y)
+        arr[x][y] = 'b';
+        updateArr(x,y)
+        
+        //console.log(arr)
         setIsDrawing(true)
     }
 
     //Stop drawing and reset the useState   
     const endDraw = () =>{
-        console.log("Ending")
-        console.log(arr)
+       // console.log("Ending")
         setIsDrawing(false)
     }
 
@@ -87,23 +93,22 @@ const Grid = () => {
     const draw = ({nativeEvent}) =>{
         if(isDrawing){
             const {offsetX, offsetY} = nativeEvent
-            console.log(offsetX + " , " + offsetY )
-            //console.log(Math.floor(offsetX/((n-6)/2))+ " , " + Math.floor(offsetY/((n-6)/2)))
-            arr[Math.floor(offsetX/scaling)][Math.floor(offsetY/scaling)] = 1;
-            console.log("Drawing")
+            const x = Math.floor(offsetX/scalingX)
+            const y = Math.floor(offsetY/scalingY)
+            //console.log(x+ " , " + y)
+            arr[x][y] = 'b';
+            updateArr(x,y)
         }
     }
 
     return (
-        <div>
             <canvas id = ".node"
             onMouseDown = {startDraw}
             onMouseUp = {endDraw}
             onMouseMove = {draw}
             ref={canvasRef}
             />
-        </div>
-           
+     
     )
 }
 
