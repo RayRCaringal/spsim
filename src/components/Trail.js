906ce90fd3dd63a5f6f2ea5e334b8fcbdd5f8b54
RayRCaringal <rayrcaringal@gmail.com>
2021-02-28 21:39:29 -0500

    Animation changes

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -4,17 +4,18 @@ import { useTrail, a } from 'react-spring'
 const Trail =({open, items, ...props}) => {
     console.log(items)
     const trail = useTrail(items.length, {
-      config: { mass: 5, tension: 2000, friction: 200 },
+      config: { mass: 5, tension: 3000, friction: 200},
       opacity: open ? 0: 1,
-      width: open ? 0 : 1000,
-      from:{opacity: 0, width: 0}
+      width: open ? 98 : 100,
+      from:{opacity: 0, width: 98}
     })
     return (
         <div {...props}>
-          <div  >
+          <div>
               {trail.map(({width:w, ...rest},index) => (
-                <a.div className = "test" key = {items[index]}>
-                    <a.svg viewBox = "0 0 1000 1000"  style = {{ ...rest, width: w.interpolate((w) =>`px`)}}>
+                <a.div style = {{width: w.interpolate((w) =>`${w}%`)}}
+                className = "test" key = {items[index]}>
+                    <a.svg viewBox = "0 0 1000 1000"  style = {{ ...rest}}>
                         {items[index]}
                     </a.svg>
                 </a.div>
